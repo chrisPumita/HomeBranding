ca57bc86c7a4c608d272b71e4a8275412a14907e
chrispumita <christian.floppy@gmail.com>
2018-04-19 09:11:25 -0500

    ESQUELETO FASE 1
    Estructura basica del HTML iconos y algunos elementos

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<HTML lang="es">
+<HEAD>
+	<TITLE>Home Branding - Inicio</TITLE>
+	<!--  INICIO METAS 	-->
+	<meta charset="utf-8">
+	<!--   FIN METAS 	-->
+	
+	<!--   INICIO ESTILOS		-->
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+	<!--    FIN ESTILOS			-->
+
+
+</HEAD>
+<BODY>
+	<HEADER>
+		<div>
+			<img class="iconHome" src="img/homebranding-logo.png">
+		</div>
+
+		<div>
+			<ul id="menu" class="navegacion">
+				<li><a class="txt-menu" href="index.php" onclick="return false";>☰ MENU</a>
+					<ul id="submenu" >
+						<li class="submenu"><a href="#">INICIO</a></li>
+						<li class="submenu"><a href="#">SERVICIOS</a></li>
+						<li class="submenu"><a href="#">¿QUIÈNES SOMOS?</a></li>
+						<li class="submenu"><a href="#">CUENTAS</li>
+						<li class="submenu"><a href="#">CONTACTO</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<div>
+			<div>
+				<h2>Siguenos!</h2>
+			</div>
+			<div>
+				<img class="redesIcons" src="img/icon-face.png">
+				<img class="redesIcons" src="img/icon-tw.png">
+			</div>
+		</div>
+
+		<div class="lineTop">
+		</div>
+	</HEADER>
+	
+
+	<MAIN>
+		
+	</MAIN>
+		<div>
+			<article class="fluid gallery">
+      			<h2>CASOS DE EXITO</h2>
+      				<figure class=""><img src="" alt="IMG1">
+        			<div class="fluid superCate"></div>
+      				</figure>
+    		</article>
+			<h2>CASOS DE ÈXITO</h2>
+		</div>
+
+
+		<div>
+			
+		</div>
+
+
+		<div>
+			
+		</div>
+
+
+		<div>
+			
+		</div>
+
+
+		<div>
+			
+		</div>
+
+	<FOOTER>
+		
+	</FOOTER>
+</BODY>
+</HTML>
