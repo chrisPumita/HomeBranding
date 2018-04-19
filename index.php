6be9b4a8fc0b4a8e8bacecd201eeb0394b699948
chrispumita <christian.floppy@gmail.com>
2018-04-19 14:29:01 -0500

    Esqueleto Terminado
    Incorporacion HTML BASICO

--- index.php
+++ index.php
@@ -13,75 +13,210 @@
 
 </HEAD>
 <BODY>
-	<HEADER>
+	<HEADER id="header">
 		<div>
-			<img class="iconHome" src="img/homebranding-logo.png">
-		</div>
-
-		<div>
-			<ul id="menu" class="navegacion">
-				<li><a class="txt-menu" href="index.php" onclick="return false";>☰ MENU</a>
-					<ul id="submenu" >
-						<li class="submenu"><a href="#">INICIO</a></li>
-						<li class="submenu"><a href="#">SERVICIOS</a></li>
-						<li class="submenu"><a href="#">¿QUIÈNES SOMOS?</a></li>
-						<li class="submenu"><a href="#">CUENTAS</li>
-						<li class="submenu"><a href="#">CONTACTO</a></li>
-					</ul>
-				</li>
-			</ul>
-		</div>
-
-		<div>
-			<div>
-				<h2>Siguenos!</h2>
+			<div id="logo">
+				<img class="iconHome" src="img/homebranding-logo.png">
 			</div>
-			<div>
-				<img class="redesIcons" src="img/icon-face.png">
-				<img class="redesIcons" src="img/icon-tw.png">
-			</div>
-		</div>
 
-		<div class="lineTop">
+			<div id="menuDesp">
+				<ul id="menu" class="navegacion">
+					<li>
+						<a class="primaryColor listMenu" href="index.php" onclick="return false";>☰</a>
+						<a class="secondColor listMenu menuTxtFormat" href="index.php" onclick="return false";><b>MENU</b></a>
+						<ul id="submenu"  class="primaryColor">
+							<li class="primaryColor decoraMenu"><a class="class_a_href menuTxtFormat" href="#">INICIO</a></li>
+							<li class="primaryColor decoraMenu"><a class="class_a_href menuTxtFormat" href="#">SERVICIOS</a></li>
+							<li class="primaryColor decoraMenu"><a class="class_a_href menuTxtFormat" href="#">¿QUIÈNES SOMOS?</a></li>
+							<li class="primaryColor decoraMenu"><a class="class_a_href menuTxtFormat" href="#">CUENTAS</li>
+							<li class="primaryColor decoraMenu"><a class="class_a_href menuTxtFormat" href="#">CONTACTO</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+
+			<div id="socialConect">
+				<div>
+					<h2 class="secondColor alingDer">Siguenos!</h2>
+				</div>
+				<div>
+					<img class="redesIcons" src="img/icon-face.png">
+					<img class="redesIcons" src="img/icon-tw.png">
+				</div>
+			</div>
+			<div class="lineTop">
+			</div>
 		</div>
 	</HEADER>
 	
 
-	<MAIN>
+	<SECTION id="section">
+		<div>
+  			<h2 class="secondColor alingDer">CASOS DE ÉXITO</h2>
+  			<div>
+  				<div>
+  					<img class="iconHome alingDer" src="img/f4dfa017-0c27-411f-87b2-1d406f74704b.JPG" alt="fotoTemporal">
+  					<img class="iconHome alingDer" src="img/e207edbc-663b-4891-8822-d991542290ee.JPG" alt="fotoTemporal">
+  					<img class="iconHome alingDer" src="img/e3c4547a-60c2-4396-b61c-5a8f37364603.JPG" alt="fotoTemporal">
+  				</div>
+  			</div>
+		</div>
+			
+		<div>
+			<div>
+				<h2>SERVICIOS</h2>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>SALMPLING</h3>
+					<p>Compuesto de promotores repartiendo volantes en puntos estratégicos seleccionados.</p>
+				</div>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>SIGN HUMAN</h3>
+					<p>Servicio con gran visibilidad, atrae clientes por la característica de que señala el punto de venta, conformado por una cedula de 2 a 4 promotores.</p>
+				</div>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>PUBLIANDANTE</h3>
+					<p>Promotor portando un mini bilboard con publicidad frente y vuelta, volanteando en zonas de gran afluencia, cercanos al punto de venta, cruceros, etc.</p>
+				</div>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>PUBLIPANTALLA</h3>
+					<p>Promotor portando un mini bilboard con publicidad frente y vuelta, volanteando en zonas de gran afluencia, cercanos al punto de venta, cruceros, etc.</p>
+				</div>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>CROSS WALKER</h3>
+					<p>Caracterizado por su gran impacto ya que además de la imagen promocional que lleve, puede repartir folleros o cualquier articulo promocional.</p>
+				</div>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>CROSS BOARD STREET</h3>
+					<p>Cédula de 4 promotoras o promotores portando un mini bilboard, recomendado para zonas de alto impacto.</p>
+				</div>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>ROLL UP</h3>
+					<p>Por medio de la exhibición de una lona publicitaria y del volanteo de flayers se logra la combinación perfecta para posicionar si marca en puntos con mayor flujo vehicular.</p>
+				</div>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>BICI VALLA</h3>
+					<p>Este servicio se caracteriza por tener la flexibilidad de recorrer y distribuir publicidad en puntos de gran afluencia, genera impacto en autos y peatones.</p>
+				</div>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>CARTELERA MOVIL</h3>
+					<p>Con un espacio publicitario, se propone al cliente rutas de acuerdo con sus necesidades.</p>
+				</div>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>ESPECTACULARES</h3>
+					<p>Contamos con los espectaculares con mayor vista dentro de la Ciudad de México, puntos como son, Santa Fe, Periférico, Viaducto, Ríos Churubusco, etc. </p>
+				</div>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>PUBLICIDAD EN RUTA</h3>
+					<p>Publicidad en movimiento, generando a diario grandes impactos ya que contamos con las rutas mas importantes, localizadas dentro de los cruces mas significativos de la CDMX.</p>
+				</div>
+				<div>
+					<img src="img/iconService.png" class="iconService">
+					<h3>MATERIAL PUBLICITARIO</h3>
+					<p>Material publicitario para punto de venta, elaborados con creatividad y solidas estructuras, listas para destacar la marca.</p>
+				</div>
+			</div>
+		</div>
+
+
+		<div>
+			<h2>QUIENES SOMOS</h2>
+			<p>
+				Tu Equipo de expertos en: Street Marketing, actividades BTL, Guerrilla en Semáforos; para lograr que su marca, su producto o empresa logre los resultados esperados a través de tácticas de mercadotecnia directa, aprovechando las oportunidades del mercado actual y futuro.
+			</p>
+			
+		</div>
+
+
+		<div>
+			<h2>CUENTAS</h2>
+
+		</div>
+
+
+		<div>
+			<div>
+				<h3>HomeBrandig</h3><br>
+				10 años ·
+			</div>
+			<div>
+				<p>
+					¡Lo + importante de la comunicación no esta en como se dice, sino en como se hace!
+				</p>
+				<p>
+					#TuPublicidadEnBuenas					
+				</p>
+			</div>
+		</div>
 		
-	</MAIN>
 		<div>
-			<article class="fluid gallery">
-      			<h2>CASOS DE EXITO</h2>
-      				<figure class=""><img src="" alt="IMG1">
-        			<div class="fluid superCate"></div>
-      				</figure>
-    		</article>
-			<h2>CASOS DE ÈXITO</h2>
+			<div>
+				<h3>Escríbenos</h3>
+				<div class="formuls">
+					<form id="contacto" name="contacto" action="send.php" method="post">
+						<div class="formulario">
+							<label for="nombre"> Nombre:</label>
+							<input  required autocomplete="off" placeholder="Escriba su nombre" type="text" name="nombre" id="nombre" maxlength="100"/>
+							<br>
+							<label for="email">Correo:</label>
+							<input  required autocomplete="off" placeholder="Escriba su correo" type="email" name="email" id="email" maxlength="120"/>
+							<br>
+							<label for="telefono">Teléfono:</label>
+							<input  required autocomplete="off" placeholder="" type="tel" name="tel" id="tel" maxlength="15" pattern="[0-9]"/>
+							<br>
+							<label>Comentario </label>
+							<textarea required name="comentario" placeholder="Escibanos su comentario" id="comentario" cols="25" rows="5"></textarea>
+							<br>
+							<!--
+							<p class="condiciones">
+								<input name="aceptaterminos" type="checkbox" id="aceptaterminos" value="si" checked=""> He leido los Términos y Politicas de Privacidad. 
+							</p>
+
+							<p class="condiciones">
+								<input name="aceptapromociones" type="checkbox" id="aceptapromociones" value="si" checked=""> Me gustaría recibir material promocional de Directa a Casa.
+							</p>
+							-->
+							<div class="btnEnviar">
+								<input class="boton hvr-float-shadow" type="submit" name="enviar" id="enviar" value="Enviar">
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+
+			<div>
+			<h3>Informes:</h3>
+			<p>Cerrada Noche Buena 15, <br>
+			Col. Potreros, Deleg. Tlalpan <br>
+			C.P. 01780, CDMX, Méx. <br>
+			<p>atencionaclientes@homebramding.com.mx</p>
+			<p>
+				35-36-32-53 <br>
+				35-36-32-54
+			</p>
+			</div>
 		</div>
-
-
-		<div>
-			
-		</div>
-
-
-		<div>
-			
-		</div>
-
-
-		<div>
-			
-		</div>
-
-
-		<div>
-			
-		</div>
+	</SECTION>
 
 	<FOOTER>
-		
+		<div>
+			<samp>
+				<!-- Implements automatic YEAR -->
+				<?php date_default_timezone_set('GMT'); echo date("Y.");?>
+				Todos los Derechos Reservados. HomeBranding S.A. de C.V.
+			</samp>
+		</div>
 	</FOOTER>
+
 </BODY>
 </HTML>
